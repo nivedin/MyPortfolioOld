223e21dacb1788390a11a19ac83fd76a72d365b0
Nivedin <nivedinp@gmail.com>
2020-04-28 22:29:09 +0530

    Portfolio making 1

---
+++ cursor.js
@@ -0,0 +1,13 @@
+const mouseCursor = document.querySelector(".mouseCursor");
+
+document.addEventListener("mousemove",e=>{
+    mouseCursor.setAttribute("style","top: "+(e.pageY - 10)+"px; left:"+(e.pageX - 10)+"px;");
+})
+
+document.addEventListener("click",() =>{
+    mouseCursor.classList.add("expand");
+
+    setTimeout(()=>{
+        mouseCursor.classList.remove("expand");
+    },500);
+})
